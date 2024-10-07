2fba0c827668f2dfaf39928ed79cee3fe1f32ea5
Arifin-only <arifin180104@gmail.com>
2024-10-07 20:29:13 +0800

    Create Map.dart

---
+++ Map.dart
@@ -0,0 +1,9 @@
+ void main(){
+   Map <String, String> nama ={};
+   var nama1 = Map<String, String>();
+   var nama2 = <String, String>{};
+
+   print(nama);
+   print(nama1);
+   print(nama2);
+ }
